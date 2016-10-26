24cd4275eda92cf8d83d184f32608f3b126033ef
Daniel Cheley <danielcheley@hotmail.com>
2016-10-26 17:16:39 -0400

    Create contacts.erb and begin Contacts page

--- crm.rb
+++ crm.rb
@@ -1,8 +1,18 @@
+require relative 'contact'
 require 'sinatra'
+
+#Fake data
+Contact.create('John', 'Google', 'test@email.com', 'Unemployed')
+Contact.create('Luis', 'Bon', 'imagine@something.com', 'Fire Fighter')
+Contact.create('Monique', 'Camarino', 'email@hotmail.com', 'Waitress')
+
 
   get '/' do
     @crm_app_name = "Daniel's CRM"
+    @server_time = Time.now
     erb :index
   end
 
-  
+  get '/contacts' do
+    erb :contacts
+  end
